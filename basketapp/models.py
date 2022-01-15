b755cf9dd30c1ef67faad114562d256bd3c5f828
emagged <ariulashmbgerdo@gmail.com>
2022-01-15 22:56:08 +0700

    Products price and amount migrated to model staticmethods

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django import template
 
 from mainapp.models import Product
 
@@ -9,3 +10,17 @@ class Basket(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(verbose_name="количество", default=0)
     add_datetime = models.DateTimeField(verbose_name="время добавления", auto_now_add=True)
+
+    @staticmethod
+    def get_products_amount(user):
+        amount = 0
+        for product in Basket.objects.filter(user=user):
+            amount += product.quantity
+        return amount
+
+    @staticmethod
+    def get_total_price(user):
+        total = 0
+        for product in Basket.objects.filter(user=user):
+            total += product.amount * product.product.price
+        return total

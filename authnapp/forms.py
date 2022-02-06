7a26b31ba3e64d3b2ae05f71893dd0fc525cf17b
HMBGERDO <61080616+HMBGERDO@users.noreply.github.com>
2022-02-06 12:21:47 +0700

    M2 lesson1 (#9)
    * lesson_9. URLs as regexp
    
    * lesson_9. Send email to file.
    
    * lesson_9. Context processor.
    
    * Lesson 9. Code style fix
    
    * M2 lesson2 (#10)
    
    * lesson_10. Social auth
    
    * lesson_10. User's profile
    
    * lesson_10. User's profile edit
    
    * Lesson 10. Code style fix
    
    * fixed categories and products templates

--- authnapp/forms.py
+++ authnapp/forms.py
@@ -1,7 +1,10 @@
+import hashlib
+import random
+
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 
-from .models import ShopUser
+from .models import ShopUser, ShopUserProfile
 
 
 class ShopUserLoginForm(AuthenticationForm):
@@ -24,15 +27,19 @@ class ShopUserRegisterForm(UserCreationForm):
 
     def clean_age(self):
         data = self.cleaned_data["age"]
-        if data < 18 or data > 120:
-            raise forms.ValidationError("Некорректный возраст!")
+        if data < 18:
+            raise forms.ValidationError("Вы слишком молоды!")
         return data
 
-    def clean_first_name(self):
-        data = self.cleaned_data["first_name"]
-        if data == "testname":
-            raise forms.ValidationError("Test name error. Choose other name.")
-        return data
+    def save(self):
+        user = super(ShopUserRegisterForm, self).save()
+
+        user.is_active = False
+        salt = hashlib.sha1(str(random.random()).encode("utf8")).hexdigest()[:6]
+        user.activation_key = hashlib.sha1((user.email + salt).encode("utf8")).hexdigest()
+        user.save()
+
+        return user
 
     class Meta:
         model = ShopUser
@@ -56,3 +63,14 @@ class ShopUserEditForm(UserChangeForm):
     class Meta:
         model = ShopUser
         fields = ("username", "first_name", "email", "age", "avatar")
+
+
+class ShopUserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = ShopUserProfile
+        fields = ("tagline", "aboutMe", "gender")
+
+    def __init__(self, *args, **kwargs):
+        super(ShopUserProfileEditForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs["class"] = "form-control"
